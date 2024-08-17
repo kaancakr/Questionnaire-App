92bd33673579adea6eba1f8b7351ca29eeff9d29
kaancakr <eren.cakir@tedu.edu.tr>
2024-08-17 16:43:04 +0300

    All changes added for question modal

---
+++ Onyx Code/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  Onyx Code
+//
+//  Created by Kaan Çakır on 17.08.2024.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
