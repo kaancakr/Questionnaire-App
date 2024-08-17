07cd46a717f035ca222d29f07c890fbe1ee44a91
kaancakr <eren.cakir@tedu.edu.tr>
2024-08-17 16:43:34 +0300

    New game modal and methods created

--- Onyx Code/Question.swift
+++ Onyx Code/Question.swift
@@ -7,9 +7,91 @@
 
 import Foundation
 
-struct Question {
+struct Question: Hashable {
     let questionText: String
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
-    var questionIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0),
+        Question(questionText: "Which programming language is known as the 'mother of all languages'?",
+                 possibleAnswers: [
+                    "C",
+                    "FORTRAN",
+                    "Assembly",
+                    "Lisp"
+                 ],
+                 correctAnswerIndex: 1),
+        Question(questionText: "Which of the following is not a high-level programming language?",
+                 possibleAnswers: [
+                    "C++",
+                    "Python",
+                    "JavaScript",
+                    "Assembly"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "Who is the creator of the Linux operating system?",
+                 possibleAnswers: [
+                    "Bill Gates",
+                    "Mark Zuckerberg",
+                    "Linus Torvalds",
+                    "Steve Jobs"
+                 ],
+                 correctAnswerIndex: 2),
+        Question(questionText: "What year was the Python programming language first released?",
+                 possibleAnswers: [
+                    "1989",
+                    "1991",
+                    "1995",
+                    "2000"
+                 ],
+                 correctAnswerIndex: 1),
+        Question(questionText: "What does 'HTML' stand for?",
+                 possibleAnswers: [
+                    "HyperText Markup Language",
+                    "Hyperlink and Text Markup Language",
+                    "Home Tool Markup Language",
+                    "Hyperlink Text Management Language"
+                 ],
+                 correctAnswerIndex: 0),
+        Question(questionText: "Which of the following is a front-end web development framework?",
+                 possibleAnswers: [
+                    "Django",
+                    "Laravel",
+                    "React",
+                    "Flask"
+                 ],
+                 correctAnswerIndex: 2),
+        Question(questionText: "Which company developed the Java programming language?",
+                 possibleAnswers: [
+                    "Microsoft",
+                    "Apple",
+                    "Sun Microsystems",
+                    "Google"
+                 ],
+                 correctAnswerIndex: 2),
+        Question(questionText: "Which version control system is widely used for managing source code?",
+                 possibleAnswers: [
+                    "SVN",
+                    "Git",
+                    "Mercurial",
+                    "CVS"
+                 ],
+                 correctAnswerIndex: 1)
+    ]
 }
--- Onyx Code/SummaryView.swift
+++ Onyx Code/SummaryView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct SummaryView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    let correctGuesses: Int
+    let incorrectGuesses: Int
+    let onPlayAgain: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Game Over")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+                .foregroundColor(accentColor)
+            
+            Text("Correct Answers: \(correctGuesses)")
+                .font(.title)
+                .foregroundColor(.green)
+                .padding(.top)
+                .bold()
+            
+            Text("Incorrect Answers: \(incorrectGuesses)")
+                .font(.title)
+                .foregroundColor(.red)
+                .padding(.top)
+                .bold()
+            
+            Button(action: {
+                onPlayAgain()
+            }) {
+                Text("Play Again")
+                    .padding()
+                    .background(accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .fontWeight(.heavy)
+            }
+            .padding(.top)
+        }
+        .padding()
     }
 }
 
 #Preview {
-    SummaryView()
+    SummaryView(correctGuesses: 3, incorrectGuesses: 2, onPlayAgain: {})
 }
