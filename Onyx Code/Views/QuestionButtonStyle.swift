94f1acc2b73dd0ba1d179a9d26b9516e5c7258df
kaancakr <eren.cakir@tedu.edu.tr>
2024-08-20 12:48:21 +0300

    Question modal button view updated

---
+++ Onyx Code/Views/QuestionButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionButtonStyle.swift
+//  Onyx Code
+//
+//  Created by Kaan Çakır on 20.08.2024.
+//
+
+import SwiftUI
+
+struct QuestionButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionButtonStyle()
+}
